33239537139f0c8c0a9408b106c14be63f306e59
TheRitikDubey <www.ritikdubey@gmail.com>
2023-02-27 21:10:02 +0530

    lib drop down added

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 let skillDorpdown=document.querySelectorAll(".skill-dropdown")
 let language=document.querySelector(".language")
+let libraries=document.querySelector(".dropDown-lib")
+let tools=document.querySelector(".Tools")
 let skill_drop_down=0;
 function showSkills() {
     console.log("working");
@@ -16,4 +18,22 @@ function showSkills() {
     
     
 }
+
+let libraries_drop_down=0;
+function showLibraries() {
+    console.log("working");
+    if(libraries_drop_down===0){
+        
+        //document.getElementById("mylib").style.transition = " visibility 2s";
+        document.getElementById("mylib").style.display = "flex";
+        libraries_drop_down=1;
+    }
+    else{
+        document.getElementById("mylib").style.display = "none";
+        libraries_drop_down=0;
+    }
+    
+    
+}
 language.addEventListener("click",showSkills);
+libraries.addEventListener("click",showLibraries);
